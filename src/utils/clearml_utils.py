3ef6b4937b01045d4cd101d579b1bd50fa1d22a2
muhammadagfian96 <muhammadagfian96@gmail.com>
2023-08-24 09:40:26 +0700

    update template

--- src/utils/clearml_utils.py
+++ src/utils/clearml_utils.py
@@ -21,7 +21,7 @@ def init_clearml():
 
     task.set_script(
         repository="https://github.com/muhammadAgfian96/template-yolov8-clearml.git",
-        branch="public",
+        branch="ikan",
         working_dir=".",
         entry_point="src/train.py",
     )
